f59eb7b488b4a23bc74b4f168cab789e67c997bc
rehanarroihan <third2014project@gmail.com>
2019-12-20 15:41:14 +0700

    added report section

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -24,7 +24,7 @@
         <!-- small card -->
         <div class="small-box bg-info">
           <div class="inner">
-            <h3>150</h3>
+            <h3>9999</h3>
 
             <p>Pegawai</p>
           </div>
@@ -41,7 +41,7 @@
         <!-- small card -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>4</h3>
+            <h3>9999</h3>
 
             <p>Pekerjaan Berjalan Saat Ini</p>
           </div>
@@ -55,7 +55,6 @@
       </div>
     </div>
   </div>
-  <!-- /.container-fluid -->
 </section>
 <!-- /.content -->
 <div class="content-header">
@@ -70,8 +69,141 @@
   </div>
 </div>
 <!-- /.content-header -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Report Pekerjaan Petugas</h3>
+      </div>
+      <div class="card-body">
+        <div class="form-group">
+          <label>Filter Range Waktu</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="far fa-clock"></i></span>
+            </div>
+            <input type="text" class="form-control float-right" id="reservationtime">
+          </div>
+        </div>
+        <!-- result info -->
+        <h6 class="mb-2"><b>Summary</b></h6>
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-info"><i class="fa fa-tools"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Maintenance</span>
+                <span class="info-box-number">9999</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-success"><i class="fa fa-tv"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Installation</span>
+                <span class="info-box-number">9999</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-warning"><i class="fas fa-hand-paper"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Preventive</span>
+                <span class="info-box-number">9999</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-danger"><i class="fa fa-walking"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Visit</span>
+                <span class="info-box-number">9999</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-warning"><i class="fa fa-broadcast-tower"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">BTS</span>
+                <span class="info-box-number">9999</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+        </div>
+
+        <table id="reportTable" class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Petugas</th>
+              <th>Jenis Pekerjaan</th>
+              <th>Waktu Mulai</th>
+              <th>Waktu Berakhir</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Ahmad Shobirin</td>
+              <td>Maintenance</td>
+              <td>Kamis, 20 Januari 2020 14.00</td>
+              <td>Kamis, 20 Januari 2020 19.00</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Ahmad Munirus</td>
+              <td>BTS</td>
+              <td>Kamis, 20 Januari 2020 12.00</td>
+              <td>Kamis, 20 Januari 2020 16.00</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Hidayat Kurnio</td>
+              <td>Visit</td>
+              <td>Kamis, 20 Januari 2020 08.00</td>
+              <td>Kamis, 20 Januari 2020 10.00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</section>
 </div>
 <script>
+document.addEventListener('DOMContentLoaded', function(){ 
+  $('#reportTable').DataTable();
+  $('#reservationtime').daterangepicker({
+    timePicker: true,
+    timePickerIncrement: 30,
+    locale: {
+      format: 'MM/DD/YYYY hh:mm A'
+    }
+  })
+}, false);
 
 var app = new Vue({
 	el: '#app',
